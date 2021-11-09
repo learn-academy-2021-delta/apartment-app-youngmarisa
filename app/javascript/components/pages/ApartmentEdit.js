68be8464cd29f8af7d2e33b829782a27eb80bef1
youngmarisa <marisayoung2013@gmail.com>
2021-11-09 10:30:53 -0800

    edit page-body for apartment index, edit, and new

--- app/javascript/components/pages/ApartmentEdit.js
+++ app/javascript/components/pages/ApartmentEdit.js
@@ -4,9 +4,9 @@ class ApartmentEdit extends Component {
   render() {
     
     return(
-      <>
+      <div className="page-body">
       <h3>Update Listing</h3>
-      </>
+      </div>
       
     )
   }
--- app/javascript/components/pages/ApartmentNew.js
+++ app/javascript/components/pages/ApartmentNew.js
@@ -4,9 +4,9 @@ class ApartmentNew extends Component {
   render() {
     
     return(
-      <>
+      <div className="page-body">
       <h3>Add New Listing</h3>
-      </>
+      </div>
       
     )
   }
